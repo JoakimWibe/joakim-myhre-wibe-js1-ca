7a5d28d9a84fa1d13376ee8df0819680000f7425
JoakimWibe <71314249+JoakimWibe@users.noreply.github.com>
2021-02-19 13:15:55 +0100

    1

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
 const url = "http://api.tvmaze.com/shows";
 const resultsContainer = document.querySelector(".results");
 
-
 async function getShows() {
-   
     try {
         const response = await fetch(url);
         const shows = await response.json();
@@ -25,9 +23,11 @@ getShows();
 function createHTML(shows) {
     for (let i = 0; i < shows.length; i++) {
         
-        if (i === 10) {
+        if (i === 16) {
             break;
         }
+
+        console.log(shows[i]);
 
         resultsContainer.innerHTML += `<a href="details.html?id=${shows[i].id}" class="card">
                                             <div class="details-image" style="background-image: url(${shows[i].image.medium})"></div>
